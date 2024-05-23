fixed bug, same issue with precision

src/main.py
@@ -15,9 +15,12 @@ screen = pg.Surface((screenx, screeny), pg.SRCALPHA)
 mousePos = (screenx / 2, screeny / 2)
 
 mirrors = []
-RAYS = 100
+RAYS = 1000
 REFLECTIONS = 10
+RAY_COLOR = (255, 255, 50, 100)
 frame_counter = 0
+check_count = 0
+prec = 10**-10
 
 
 class Mirror:
@@ -32,6 +35,7 @@ class Mirror:
             self.eccentricity = kwargs["offset"]
 
     def intersect(self, startPos, Vect):
+        global check_count
         p1, p2 = startPos
         v1, v2 = Vect
 
@@ -75,18 +79,32 @@ class Mirror:
                 print(q3)
             # add more checks here when using elliptic arcs
             # bug: when ray origin is inside ellipse, rays escape. when ray origin is outside ellipse, all fine
-            if m1 > 0 and m2 < 0:
+            if m2>=m1>=0:
+                print(m1, " < ", m2)
+                check_count += 1
+            elif m1>=m2>=0:
+                print(m1, " > ", m2)
+                check_count += 1
+            elif m1==m2!=0:
+                print(m1, " = ", m2)
+                check_count += 1
+                
+            if m1 > prec and m2 < prec:
                 x = o1 + c1 + m1 * v1
                 y = o2 + c2 + m1 * v2
                 collidepos_1 = (x, y)
                 return collidepos_1
-            elif m2 > 0 and m1 < 0:
+            # could implement search with small steps to compare when steps find something and intersect finds none
+            elif m2 > prec and m1 < prec:
                 x = o1 + c1 + m2 * v1
                 y = o2 + c2 + m2 * v2
                 collidepos_2 = (x, y)
                 print(m1, m2)
                 return collidepos_2
-            elif m1 > 0 and m2 > 0:
+
+            elif m1 > prec and m2 > prec:
+                # this works since m2 is smaller than m1. this logic covers
+                # the case where the origin is outside the ellipse.
                 x = o1 + c1 + m2 * v1
                 y = o2 + c2 + m2 * v2
                 collidepos = (x, y)
@@ -150,7 +168,7 @@ def rayPhysicsHandler(matrix):
 def distributor(rayMatrix):
     output = np.empty((RAYS, 6))
     errors = 0
-
+    """
     with ProcessPoolExecutor(max_workers=CORE_COUNT) as executor:
         _out = executor.map(rayPhysicsHandler, rayMatrix)
 
@@ -166,7 +184,7 @@ def distributor(rayMatrix):
             output[i] = out
         except ValueError:
             errors += 1
-    """
+    
     print(
         f"error count for frame no ({frame_counter}): {errors}"
     ) if errors > 0 else None
@@ -175,10 +193,12 @@ def distributor(rayMatrix):
 
 def render(rayMatrix, reset):
     global counter, frame_counter
-
+    temp_surf = screen.copy()
     BGCOLOR = (10, 10, 10)
     output = np.empty((RAYS, 4))
+    print(check_count)
     if reset is True:
+        print(f"Frame {frame_counter}: \n")
         counter = 0
         screen.fill(BGCOLOR)
         for i in range(RAYS):
@@ -192,6 +212,7 @@ def render(rayMatrix, reset):
                 output[i] = [startPos[0], startPos[1], vect[0], vect[1]]
 
     else:
+        print(f"Frame {frame_counter}: \n")
         for mirror in mirrors:
             mirror.draw("white", screen)
         newMatrix = distributor(rayMatrix)
@@ -204,12 +225,12 @@ def render(rayMatrix, reset):
             )
             pg.draw.line(
                 screen,
-                (255, 255, 0, 50),
+                RAY_COLOR,
                 (startPos_x, startPos_y),
                 (intersect_x, intersect_y),
             )
             output[i] = [intersect_x, intersect_y, vector_x, vector_y]
-
+        display.blit(temp_surf, (0, 0))
         display.blit(screen, (0, 0))
     pg.draw.circle(display, "orange", mousePos, 5)
     pg.display.flip()
@@ -255,9 +276,9 @@ def main():
             rayMatrix = render(rayMatrix, False)
 
         time_new = time.perf_counter()
-        print(
-            f"Time for frame({frame_counter}): {time_new - time_current:0.4f} seconds"
-        )
+        #print(
+        #    f"Time for frame({frame_counter}): {time_new - time_current:0.4f} seconds"
+        #)
         time_current = time_new
 
     sys.exit()